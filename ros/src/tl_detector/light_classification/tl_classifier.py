8a2e54c88601b6ba8fa9f07741bb501d296a3d28
David Liu <jaxanc@gmail.com>
2017-12-24 12:46:25 +1300

    updated traffic light detector

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,45 @@
 from styx_msgs.msg import TrafficLight
 
+import numpy as np
+import os
+import tensorflow as tf
+
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        # What model to download.
+        model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
+        model_name = model_name + '.tar.gz'
+
+        # Path to frozen detection graph. This is the actual model that is used for the object detection.
+        path_to_ckpt = model_name + '/frozen_inference_graph.pb'
+
+        # List of the strings that is used to add correct label for each box.
+        path_to_labels = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+        # Load a (frozen) Tensorflow model into memory
+        self.detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(path_to_ckpt, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+            self.sess = tf.Session(graph=self.detection_graph, config=config)
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            self.scores =self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections =self.detection_graph.get_tensor_by_name('num_detections:0')
+    
+    # Helper code
+    def load_image_into_numpy_array(image):
+        (im_width, im_height) = image.size
+        return np.array(image.getdata()).reshape(
+            (im_height, im_width, 3)).astype(np.uint8)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
